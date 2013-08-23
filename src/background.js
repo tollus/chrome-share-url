9526f6c10bd6622897e3f159d289021ebfc616ff
tollus <tollus@users.noreply.github.com>
2013-08-23 00:37:47 -0400

    added options screen with computer names

--- src/background.js
+++ src/background.js
@@ -17,18 +17,45 @@ function genericOnClick(info, tab) {
 }
 
 var contextMenuId;
+var menuData;
 
 // Set up context menu tree at install time.
-function createContextMenu() {
-  if(contextMenuId) return;
+function createContextMenu(force) {
+  if (contextMenuId && !force) return;
 
-	console.log("created contextMenu");
-  contextMenuId = chrome.contextMenus.create(
-        {"title": "Open on other computer(s)",
-          "contexts":['link'],
-          "onclick": genericOnClick,
-          "targetUrlPatterns":["http://*/*","https://*/*"]
-         });
+  if (contextMenuId) {
+    chrome.contextMenus.removeAll();
+  }
+  menuData = {};
+
+  chrome.storage.sync.get('computers', function(items){
+  	console.log("created contextMenu");
+    contextMenuId = chrome.contextMenus.create(
+          {"title": "Open on other computer(s)",
+            "contexts":['link'],
+            "onclick": genericOnClick,
+            "targetUrlPatterns":["http://*/*","https://*/*"]
+           });
+
+    if(items.computers && (items.computers.length > 1 || !items.computers[computerId])) {
+      child = chrome.contextMenus.create({
+        'title': 'Send to all',
+        'onclick': genericOnClick,
+        'parentId': contextMenuId
+      });
+
+      console.log('Adding child menus for computers.');
+      for (var c in items.computers) {
+        if(c != computerId) {
+          menuData[c] = chrome.contextMenus.create({
+            'title': 'Send to ' + items.computers[c],
+            'onclick': genericOnClick,
+            'parentId': contextMenuId
+          });
+        }
+      }
+    }
+  });
 };
 
 function findComputerId(){
@@ -78,22 +105,49 @@ if (chrome.tabs) {
 chrome.runtime.onInstalled.addListener(createContextMenu);
 
 chrome.storage.onChanged.addListener(function(changes, storageNamespace) {
-  if (storageNamespace != 'sync') return;
+  if (storageNamespace == 'sync') {
+    console.log('Sync\'d content incoming: ', changes);
 
-  console.log('Sync\'d content incoming: ', changes);
+    if (changes.computers) {
+      // reset the names
+      createContextMenu(true);
+      return;
+    }
 
-  chrome.storage.sync.get(function(items){
-    // invalid data set
-    if (!items.from) return;
+    if (changes.link) {
+      chrome.storage.sync.get(function(items){
+        // invalid data set
+        if (!items.from || !items.link) return;
 
-    // received my message, can be ignored
-    if (items.from == computerId) return;
+        // received my message, can be ignored
+        if (items.from == computerId) return;
 
-    console.log('Opening link to %s.', items.link);
+        var computerName = 'Unnamed ' + items.from;
+        if (items.computers && items.computers[items.from]) {
+          computerName = items.computers[items.from];
+        }
 
-    // received message from other computer, open the tab
-    chrome.tabs.create({ url: items.link });
-  });
+        console.log('Opening link to %s from computer %s.',
+            items.link, computerName);
+
+        // received message from other computer, open the tab
+        chrome.tabs.create({ url: items.link });
+      });
+    }
+  } else if (storageNamespace == 'local') {
+    if (changes.computerName) {
+      // my computer name changed, so send it to other computers
+
+      chrome.storage.sync.get(function(items){
+        items.from = computerId;
+        items.computers = items.computers || {};
+        items.computers[computerId] = changes.computerName.newValue;
+        items.link = null;
+
+        chrome.storage.sync.set(items);
+      });
+    }
+  }
 });
 
 findComputerId();
---
+++ src/options.js
@@ -0,0 +1,48 @@
+;(function($, undefined){
+
+	var $computerName;
+
+	var settings;
+
+	$(init);
+
+	function init(){
+		$computerName = $('#computer-name');
+		$saveButton = $('#save-button').button().on('click', saveChanges);
+
+		chrome.storage.local.get(null, function(items){
+			var changed = false;
+
+			settings = $.extend({}, items);
+
+			if (!settings.computerName) {
+				settings.computerName = "Unnamed " + settings.computerId;
+				changed = true;
+			}
+
+			if (changed) {
+				saveSettings();
+			}
+
+			$computerName.val(settings.computerName);
+		});
+	}
+
+	function saveChanges(e) {
+		settings.computerName = $computerName.val();
+
+		saveSettings();
+	}
+
+	function saveSettings() {
+		chrome.storage.local.set(settings, function() {
+		    var error = chrome.runtime ?
+		                chrome.runtime.lastError : chrome.extension.lastError;
+			if (error) {
+				console.error('Error occurred changing settings: %s', error);
+				alert('Error occurred changing settings: ' + error);
+			}
+		});
+	}
+
+}(jQuery));
